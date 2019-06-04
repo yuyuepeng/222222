e7af5f457e2d11e5f7766723e10f32d1d10ffe36
yuyuepeng <616250176@qq.com>
2019-06-05 01:04:08 +0800

    no message

---
+++ 22222/LinkList/LinkList.h
@@ -0,0 +1,24 @@
+//
+//  LinkList.h
+//  22222
+//
+//  Created by 扶摇先生 on 2019/6/3.
+//  Copyright © 2019年 玉岳鹏. All rights reserved.
+//
+
+#import "AbstractList.h"
+NS_ASSUME_NONNULL_BEGIN
+@interface Node : NSObject
+
+@end
+
+@implementation Node
+
+
+
+@end
+@interface LinkList : AbstractList
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 22222/LinkList/LinkList.m
@@ -0,0 +1,19 @@
+//
+//  LinkList.m
+//  22222
+//
+//  Created by 扶摇先生 on 2019/6/3.
+//  Copyright © 2019年 玉岳鹏. All rights reserved.
+//
+
+#import "LinkList.h"
+
+
+
+@interface LinkList()
+
+@end
+
+@implementation LinkList
+
+@end

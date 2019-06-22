da8442fb93cc7358aa24064ff956d57473b444ba
yuyuepeng <616250176@qq.com>
2019-06-22 14:46:52 +0800

    no message

--- 22222/tools/UIButton+YYPExtension.h
+++ 22222/tools/UIButton+YYPExtension.h
@@ -13,10 +13,24 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIButton (YYPExtension)
 
+/**
+ 存放颜色和状态字符串的字典
+ */
 @property(nonatomic, strong) NSMutableDictionary *stateBackgroundColor;
 
+/**
+ 根据状态设置背景颜色
+
+ @param backgroundColor 背景色
+ @param state 状态
+ */
 - (void)setBackgroundColor:(UIColor *)backgroundColor state:(UIControlState)state;
 
+/**
+ 设置不可点击下的背景色
+
+ @param backgroundColor 背景色
+ */
 - (void)setUserInteractionDisabledBackgroundColor:(UIColor *)backgroundColor;
 @end
 

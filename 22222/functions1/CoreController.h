6e506678f258479fc29cdd8dba28651fa799ba31
yuyuepeng <yuyuepeng@singsound.com>
2021-09-29 10:42:04 +0800

    删除阿里demo

---
+++ 22222/functions1/CoreController.h
@@ -0,0 +1,17 @@
+//
+//  CoreController.h
+//  22222
+//
+//  Created by tangsanduo on 2021/9/28.
+//  Copyright © 2021 玉岳鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CoreController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 22222/functions1/CoreController.m
@@ -0,0 +1,32 @@
+//
+//  CoreController.m
+//  22222
+//
+//  Created by tangsanduo on 2021/9/28.
+//  Copyright © 2021 玉岳鹏. All rights reserved.
+//
+
+#import "CoreController.h"
+
+@interface CoreController ()
+
+@end
+
+@implementation CoreController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
